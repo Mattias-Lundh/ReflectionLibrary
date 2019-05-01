f008a671ae3e26ed01bda71fda5c762bfb6a12ff
Mattias Lundh <Mattiasanderslundh@gmail.com>
2019-05-01 14:38:48 +0200

    no change

--- ReflectionLibrary/DesignPatterns/Null Object/Demo/NullObjectDemo.cs
+++ ReflectionLibrary/DesignPatterns/Null Object/Demo/NullObjectDemo.cs
@@ -8,7 +8,7 @@ namespace ReflectionLibrary.DesignPatterns.Null_Object.Demo
 {
     public class NullObjectDemo : IDemo, IDemoDescription
     {
-        public string Description { get; set; } = "A no-operation object that conforms to the required interface, satsfying a dependancy requirement of some other object";
+        public string Description { get; set; } = "A no-operation object that conforms to the required interface, satsfying a dependancy requirement of some other object.";
         public string Title { get; set; } = "Null object";
 
         public void Run()
