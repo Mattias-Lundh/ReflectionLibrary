3f7b952d0ad322bdcf3b2387c0e8f4cb7ed3f85f
Mattias Lundh <Mattiasanderslundh@gmail.com>
2020-03-04 13:00:56 +0100

    tidy

--- ReflectionLibrary/Program.cs
+++ ReflectionLibrary/Program.cs
@@ -1,33 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using ReflectionLibrary.DesignPatterns.Adapter.Demo;
+using ReflectionLibrary.DesignPatterns.Bridge.Demo;
+using ReflectionLibrary.DesignPatterns.Builder.Demo;
+using ReflectionLibrary.DesignPatterns.ChainOfResponsibility.Demo;
+using ReflectionLibrary.DesignPatterns.Command.Demo;
+using ReflectionLibrary.DesignPatterns.Composite.Demo;
+using ReflectionLibrary.DesignPatterns.Decorator.Demo;
+using ReflectionLibrary.DesignPatterns.Facade.Demo;
+using ReflectionLibrary.DesignPatterns.Factory.Demo;
+using ReflectionLibrary.DesignPatterns.FlyWeight.Demo;
+using ReflectionLibrary.DesignPatterns.Interpreter.Demo;
+using ReflectionLibrary.DesignPatterns.Iterator.Demo;
+using ReflectionLibrary.DesignPatterns.Mediator.Demo;
+using ReflectionLibrary.DesignPatterns.Memento.Demo;
+using ReflectionLibrary.DesignPatterns.Null_Object.Demo;
+using ReflectionLibrary.DesignPatterns.Observer.Demo;
+using ReflectionLibrary.DesignPatterns.Prototype.Demo;
+using ReflectionLibrary.DesignPatterns.Proxy.Demo;
+using ReflectionLibrary.DesignPatterns.Singleton.Demo;
+using ReflectionLibrary.DesignPatterns.State.Demo;
+using ReflectionLibrary.DesignPatterns.Strategy.Demo;
+using ReflectionLibrary.DesignPatterns.Template_Method.Demo;
+using ReflectionLibrary.DesignPatterns.Visitor.Demo;
+using ReflectionLibrary.Infrastructure;
 
 namespace ReflectionLibrary
 {
-    using System.Collections.Generic;
-    using ReflectionLibrary.DesignPatterns.Adapter.Demo;
-    using ReflectionLibrary.DesignPatterns.Bridge.Demo;
-    using ReflectionLibrary.DesignPatterns.Builder.Demo;
-    using ReflectionLibrary.DesignPatterns.ChainOfResponsibility.Demo;
-    using ReflectionLibrary.DesignPatterns.Command.Demo;
-    using ReflectionLibrary.DesignPatterns.Composite.Demo;
-    using ReflectionLibrary.DesignPatterns.Decorator.Demo;
-    using ReflectionLibrary.DesignPatterns.Facade.Demo;
-    using ReflectionLibrary.DesignPatterns.Factory.Demo;
-    using ReflectionLibrary.DesignPatterns.FlyWeight.Demo;
-    using ReflectionLibrary.DesignPatterns.Interpreter.Demo;
-    using ReflectionLibrary.DesignPatterns.Iterator.Demo;
-    using ReflectionLibrary.DesignPatterns.Mediator.Demo;
-    using ReflectionLibrary.DesignPatterns.Memento.Demo;
-    using ReflectionLibrary.DesignPatterns.Null_Object.Demo;
-    using ReflectionLibrary.DesignPatterns.Observer.Demo;
-    using ReflectionLibrary.DesignPatterns.Prototype.Demo;
-    using ReflectionLibrary.DesignPatterns.Proxy.Demo;
-    using ReflectionLibrary.DesignPatterns.Singleton.Demo;
-    using ReflectionLibrary.DesignPatterns.State.Demo;
-    using ReflectionLibrary.DesignPatterns.Strategy.Demo;
-    using ReflectionLibrary.DesignPatterns.Template_Method.Demo;
-    using ReflectionLibrary.DesignPatterns.Visitor.Demo;
-    using ReflectionLibrary.Infrastructure;
-
     class Program
     {
         static void Main(string[] args)
